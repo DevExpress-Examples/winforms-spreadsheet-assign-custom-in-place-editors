456df9fdf2c447fbb5dcc0b67e959276d863ca09
DevExpressExampleBot <fakeEmail>
2023-10-16 17:07:06 +0400

    Source auto update [skip ci]

--- VB/Spreadsheet_CustomCellEditors/Program.vb
+++ VB/Spreadsheet_CustomCellEditors/Program.vb
@@ -1,23 +1,22 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.LookAndFeel
 
 Namespace Spreadsheet_CustomCellEditors
-	Friend Module Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread>
-		Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
 
-			DevExpress.Skins.SkinManager.EnableFormSkins()
-			DevExpress.UserSkins.BonusSkins.Register()
-			UserLookAndFeel.Default.SetSkinStyle("Office 2016 Colorful")
+    Friend Module Program
 
-			Application.Run(New Form1())
-		End Sub
-	End Module
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            DevExpress.Skins.SkinManager.EnableFormSkins()
+            DevExpress.UserSkins.BonusSkins.Register()
+            UserLookAndFeel.Default.SetSkinStyle("Office 2016 Colorful")
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
--- VB/Spreadsheet_CustomCellEditors/Properties/AssemblyInfo.vb
+++ VB/Spreadsheet_CustomCellEditors/Properties/AssemblyInfo.vb
@@ -1,23 +1,23 @@
-﻿Imports System.Reflection
-Imports System.Runtime.CompilerServices
+Imports System.Reflection
 Imports System.Runtime.InteropServices
+
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("Spreadsheet_CustomCellEditors")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("Spreadsheet_CustomCellEditors")>
-<Assembly: AssemblyCopyright("Copyright ©  2016")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
+<Assembly:AssemblyTitle("Spreadsheet_CustomCellEditors")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("")>
+<Assembly:AssemblyProduct("Spreadsheet_CustomCellEditors")>
+<Assembly:AssemblyCopyright("Copyright ©  2016")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
-<Assembly: ComVisible(False)>
+<Assembly:ComVisible(False)>
 ' The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("d5877b19-de71-41e8-909c-6e51ddcd7f2e")>
+<Assembly:Guid("d5877b19-de71-41e8-909c-6e51ddcd7f2e")>
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
@@ -25,5 +25,5 @@ Imports System.Runtime.InteropServices
 '      Build Number
 '      Revision
 '
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
